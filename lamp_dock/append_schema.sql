57ca50116125f7c93babda1db51160d6de489ec7
jyunya takahashi <mickey.co.121@gmail.com>
2020-09-23 00:38:49 +0900

    テーブル設計ファイルを変更#10

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -1,14 +1,15 @@
 CREATE TABLE orders (
   order_id INT not null AUTO_INCREMENT,
-  order_date datetime not null DEFAULT CURRENT_TIMESTAMP	,
+  order_date datetime not null DEFAULT CURRENT_TIMESTAMP,
   user_id INT not null,
-  total_price INT not null,
   primary key(order_id)
 );
 
 CREATE TABLE order_items (
+  order_detail_id INT not null AUTO_INCREMENT,
   order_id INT not null,
   item_id INT not null,
   quantity INT not null,
-  order_price INT not null
+  order_price INT not null,
+  primary key(order_detail_id)
 );
